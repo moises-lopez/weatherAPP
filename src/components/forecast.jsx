00defedef8bb0dbaef8ee58eea24d8e094c4ffea
pacosw1 <pacoswilliams@gmail.com>
2021-01-03 23:38:20 -0600

    bugfix

--- src/components/forecast.jsx
+++ src/components/forecast.jsx
@@ -5,18 +5,27 @@ import "../css/forecast.css";
 const CardsForecast = () => {
   let { state: globalState, dispatch } = useContext(main);
   useEffect(() => {}, []);
-  let forecastData = globalState.forecast.data;
+  let forecastData = globalState.forecast
+
+  if (!forecastData["data"]) return null;
+
+  
   //console.log("forecast", forecastData.forecast.forecastday);
-  let forecastArray = forecastData.forecast.forecastday;
+  let forecastArray = forecastData.data.forecast.forecastday;
+
+
+  //render cards
+  let cards = forecastArray.map((day) => (
+    <article className="card">
+      <p>{day.date}</p>
+      <p>Promedio Temp : {day.day.avgtemp_c}</p>
+    </article>
+  ))
+  
   return (
     <div className="centered">
       <section className="cards">
-        {forecastArray.map((day) => (
-          <article className="card">
-            <p>{day.date}</p>
-            <p>Promedio Temp : {day.day.avgtemp_c}</p>
-          </article>
-        ))}
+       {cards}
       </section>
     </div>
   );
